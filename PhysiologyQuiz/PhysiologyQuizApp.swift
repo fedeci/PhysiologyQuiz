c318aa332bdf85bb17d23c99c0a8ac8b1e2c5edf
Federico Ciardi <fed.ciardi@gmail.com>
2022-07-11 11:24:32 +0200

    Initial Commit

---
+++ PhysiologyQuiz/PhysiologyQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhysiologyQuizApp.swift
+//  PhysiologyQuiz
+//
+//  Created by Federico Ciardi on 11/07/22.
+//
+
+import SwiftUI
+
+@main
+struct PhysiologyQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
